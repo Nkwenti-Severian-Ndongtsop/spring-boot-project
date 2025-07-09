436b728f6683e1bfc9247d95436f2b656422f66a
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-07-09 09:51:16 +0100

    feat: add main application class for Spring Boot flight application

---
+++ src/main/java/com/adorsys_gis/flight/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.adorsys_gis.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
