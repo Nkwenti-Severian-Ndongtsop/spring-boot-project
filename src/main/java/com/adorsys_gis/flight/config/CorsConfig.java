b0a990f6f523a7ef757bf1d86a19ec9457febe82
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-07-09 09:51:24 +0100

    feat: add CORS configuration for handling cross-origin requests in flight application

---
+++ src/main/java/com/adorsys_gis/flight/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.adorsys_gis.flight.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.addAllowedOriginPattern("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addExposedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
