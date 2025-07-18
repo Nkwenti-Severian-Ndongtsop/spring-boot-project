de6cbe06c58d70ced0b58129e8e3d1b5a7a21976
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-07-18 13:45:47 +0100

    feat: add WebConfig class to configure CORS settings for API endpoints

---
+++ src/main/java/com/adorsys_gis/flight/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.adorsys_gis.flight.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@org.springframework.context.annotation.Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+} 
